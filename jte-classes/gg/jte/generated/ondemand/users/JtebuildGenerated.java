71964e3c8743c232291466297bf0b8f972447f90
Olga Molkova <olgamolkova190719@gmail.com>
2024-07-06 16:30:49 +0300

    post form

--- jte-classes/gg/jte/generated/ondemand/users/JtebuildGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/users/JtebuildGenerated.java
@@ -1,9 +1,13 @@
 package gg.jte.generated.ondemand.users;
 public final class JtebuildGenerated {
 	public static final String JTE_NAME = "users/build.jte";
-	public static final int[] JTE_LINE_INFO = {27,27,27,27,27,27,27,27,27,27,27};
+	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,2,2,30,30,30,30,30,30,30,30};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
-		jteOutput.writeContent("    <form action=\"/users\" method=\"post\">\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Name\r\n                <input type=\"text\" class=\"form-control\" name=\"firstName\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Last Name\r\n                <input type=\"text\" class=\"form-control\" name=\"lastName\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Email\r\n                <input type=\"email\" class=\"form-control\" required name=\"email\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Password\r\n                <input type=\"password\" class=\"form-control\" required name=\"password\" />\r\n            </label>\r\n        </div>\r\n        <input type=\"submit\" class=\"btn btn-primary\" value=\"Sign up\" />\r\n    </form>\r\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\r\n    <form action=\"/users\" method=\"post\">\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Имя\r\n                <input type=\"text\" class=\"form-control\" name=\"firstName\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Фамилия\r\n                <input type=\"text\" class=\"form-control\" name=\"lastName\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Email\r\n                <input type=\"email\" class=\"form-control\" required name=\"email\" />\r\n            </label>\r\n        </div>\r\n        <div class=\"mb-3\">\r\n            <label class=\"form-label\">\r\n                Пароль\r\n                <input type=\"password\" class=\"form-control\" required name=\"password\" />\r\n            </label>\r\n        </div>\r\n        <input type=\"submit\" class=\"btn btn-primary\" value=\"Зарегистрировать\" />\r\n    </form>\r\n");
+			}
+		});
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
--- jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
@@ -2,22 +2,27 @@ package gg.jte.generated.ondemand.users;
 import org.example.hexlet.dto.UsersPage;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "users/index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,10,10,10,13,13,13,16,16,16,16,16,16,19,19,22,22,22,1,1,1,1};
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,3,3,5,5,13,13,16,16,16,19,19,19,19,19,19,22,22,24,24,24,24,24,1,1,1,1};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UsersPage page) {
-		jteOutput.writeContent("\r\n    <h1>Users</h1>\r\n\r\n    <div class=\"mb-3\">\r\n        <a href=\"/users/build\">Sign up a new user</a>\r\n    </div>\r\n\r\n    <table class=\"table table-striped\">\r\n        ");
-		for (var user : page.getUsers()) {
-			jteOutput.writeContent("\r\n            <tr>\r\n                <td>\r\n                    ");
-			jteOutput.setContext("td", null);
-			jteOutput.writeUserContent(user.getId());
-			jteOutput.writeContent("\r\n                </td>\r\n                <td>\r\n                    ");
-			jteOutput.setContext("td", null);
-			jteOutput.writeUserContent(user.getFirstName());
-			jteOutput.writeContent(" ");
-			jteOutput.setContext("td", null);
-			jteOutput.writeUserContent(user.getLastName());
-			jteOutput.writeContent("\r\n                </td>\r\n            </tr>\r\n        ");
-		}
-		jteOutput.writeContent("\r\n    </table>\r\n\r\n");
+		jteOutput.writeContent("\r\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\r\n    <h1>Пользователи</h1>\r\n\r\n    <div class=\"mb-3\">\r\n        <a href=\"/users/build\">Зарегистрировать нового пользователя</a>\r\n    </div>\r\n\r\n    <table class=\"table table-striped\">\r\n        ");
+				for (var user : page.getUsers()) {
+					jteOutput.writeContent("\r\n            <tr>\r\n                <td>\r\n                    ");
+					jteOutput.setContext("td", null);
+					jteOutput.writeUserContent(user.getId());
+					jteOutput.writeContent("\r\n                </td>\r\n                <td>\r\n                    ");
+					jteOutput.setContext("td", null);
+					jteOutput.writeUserContent(user.getFirstName());
+					jteOutput.writeContent(" ");
+					jteOutput.setContext("td", null);
+					jteOutput.writeUserContent(user.getLastName());
+					jteOutput.writeContent("\r\n                </td>\r\n            </tr>\r\n        ");
+				}
+				jteOutput.writeContent("\r\n    </table>\r\n");
+			}
+		});
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		UsersPage page = (UsersPage)params.get("page");
