155ec6c2125bd80d00cbf8bc6fb8c77ac2409e87
Olga Molkova <olgamolkova190719@gmail.com>
2024-07-16 20:17:01 +0300

    NamedRoutes

--- jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/users/JteindexGenerated.java
@@ -1,13 +1,23 @@
 package gg.jte.generated.ondemand.users;
+import org.example.hexlet.NamedRoutes;
 import org.example.hexlet.dto.UsersPage;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "users/index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,3,3,5,5,13,13,16,16,16,19,19,19,19,19,19,22,22,24,24,24,24,24,1,1,1,1};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,4,4,6,6,10,10,10,10,10,10,10,10,10,14,14,17,17,17,20,20,20,20,20,20,23,23,25,25,25,25,25,2,2,2,2};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UsersPage page) {
 		jteOutput.writeContent("\r\n");
 		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
 			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
-				jteOutput.writeContent("\r\n    <h1>Пользователи</h1>\r\n\r\n    <div class=\"mb-3\">\r\n        <a href=\"/users/build\">Зарегистрировать нового пользователя</a>\r\n    </div>\r\n\r\n    <table class=\"table table-striped\">\r\n        ");
+				jteOutput.writeContent("\r\n    <h1>Пользователи</h1>\r\n\r\n    <div class=\"mb-3\">\r\n        <a");
+				var __jte_html_attribute_0 = NamedRoutes.buildUsersPath();
+				if (gg.jte.runtime.TemplateUtils.isAttributeRendered(__jte_html_attribute_0)) {
+					jteOutput.writeContent(" href=\"");
+					jteOutput.setContext("a", "href");
+					jteOutput.writeUserContent(__jte_html_attribute_0);
+					jteOutput.setContext("a", null);
+					jteOutput.writeContent("\"");
+				}
+				jteOutput.writeContent(">Зарегистрировать нового пользователя</a>\r\n    </div>\r\n\r\n    <table class=\"table table-striped\">\r\n        ");
 				for (var user : page.getUsers()) {
 					jteOutput.writeContent("\r\n            <tr>\r\n                <td>\r\n                    ");
 					jteOutput.setContext("td", null);
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,16 +2,21 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 import static io.javalin.rendering.template.TemplateUtil.model;
+
+import io.javalin.validation.ValidationError;
+import io.javalin.validation.ValidationException;
 import org.example.hexlet.dto.UsersPage;
+import org.example.hexlet.model.BuildUserPage;
 import org.example.hexlet.model.User;
 import io.javalin.rendering.template.JavalinJte;
 import repository.UserRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class HelloWorld {
     public static Javalin getApp() {
-
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
@@ -21,29 +26,37 @@ public final class HelloWorld {
             ctx.render("index.jte");
         });
 
-        app.get("/users", ctx -> {
+        app.get(NamedRoutes.usersPath(), ctx -> {
             List<User> users = UserRepository.getEntities();
             var page = new UsersPage(users);
             ctx.render("users/index.jte", model("page", page));
         });
 
-        // BEGIN
-        app.get("/users/build", ctx -> {
-            ctx.render("users/build.jte");
+        app.get(NamedRoutes.buildUsersPath(), ctx -> {
+            var page = new BuildUserPage();
+            ctx.render("users/build.jte", model("page", page));
         });
 
-        app.post("/users", ctx -> {
+        app.post(NamedRoutes.usersPath(), ctx -> {
             var firstName = ctx.formParam("firstName");
             var lastName = ctx.formParam("lastName");
             var email = ctx.formParam("email").trim().toLowerCase();
-            var password = ctx.formParam("password");
-            var encryptedPassword = password;
 
-            var user = new User(firstName, lastName, email, encryptedPassword);
-            UserRepository.save(user);
-            ctx.redirect("/users");
+
+            try {
+                var passwordConfirmation = ctx.formParam("passwordConfirmation");
+                var password = ctx.formParamAsClass("password", String.class)
+                        .check(value -> value.equals(passwordConfirmation), "The passwords don't match")
+                        .check(value -> value.length() > 6, "The password is not long enough")
+                        .get();
+                var user = new User(firstName, lastName, email, password);
+                UserRepository.save(user);
+                ctx.redirect("/users");
+            } catch (ValidationException e) {
+                var page = new BuildUserPage(firstName, lastName, email, e.getErrors());
+                ctx.render("users/build.jte", model("page", page));
+            }
         });
-        // END
 
         return app;
     }
