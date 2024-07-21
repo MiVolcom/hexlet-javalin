75c1344c9df889b4617f4eb5d414a2b5cc708fd2
Olga Molkova <olgamolkova190719@gmail.com>
2024-07-21 18:20:34 +0300

    cookies

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,6 +7,7 @@ import static io.javalin.rendering.template.TemplateUtil.model;
 import io.javalin.validation.ValidationError;
 import io.javalin.validation.ValidationException;
 import org.example.hexlet.controller.UsersController;
+import org.example.hexlet.dto.MainPage;
 import org.example.hexlet.dto.UsersPage;
 import org.example.hexlet.model.BuildUserPage;
 import org.example.hexlet.model.User;
@@ -24,21 +25,27 @@ public final class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
 
+//        app.get("/", ctx -> {
+//            ctx.render("index.jte");
+//        });
+//
+//        app.get("/users", UsersController::index);
+//
+//        app.get("/users/build", UsersController::build);
+//
+//        app.post("/users", UsersController::create);
+//
+//        app.get("/users/{id}/edit", UsersController::edit);
+//
+//        app.patch("/users/{id}", UsersController::update);
+//
+//        app.delete("/users/{id}", UsersController::destroy);
         app.get("/", ctx -> {
-            ctx.render("index.jte");
+            var visited = Boolean.valueOf(ctx.cookie("visited"));
+            var page = new MainPage(visited);
+            ctx.render("mainPage.jte", model("page", page));
+            ctx.cookie("visited", String.valueOf(true));
         });
-
-        app.get("/users", UsersController::index);
-
-        app.get("/users/build", UsersController::build);
-
-        app.post("/users", UsersController::create);
-
-        app.get("/users/{id}/edit", UsersController::edit);
-
-        app.patch("/users/{id}", UsersController::update);
-
-        app.delete("/users/{id}", UsersController::destroy);
 
         return app;
     }
